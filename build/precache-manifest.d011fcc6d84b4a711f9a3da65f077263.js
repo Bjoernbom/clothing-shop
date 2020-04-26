0458a380c9877868bfed3f5c0bc1403e26236195
Willie <Willie.bjornbom@ancon.io>
2020-04-26 16:54:38 +0200

    deploy github pages

---
+++ build/precache-manifest.d011fcc6d84b4a711f9a3da65f077263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f02169028abf93cb5184fda711f808b",
+    "url": "/clothing-shop/index.html"
+  },
+  {
+    "revision": "0476121958630607617a",
+    "url": "/clothing-shop/static/css/main.ff3cb6f0.chunk.css"
+  },
+  {
+    "revision": "7133db391af46d00361a",
+    "url": "/clothing-shop/static/js/2.f181b5c0.chunk.js"
+  },
+  {
+    "revision": "0476121958630607617a",
+    "url": "/clothing-shop/static/js/main.1e3f43d9.chunk.js"
+  },
+  {
+    "revision": "a0b742ebc30099bf8619",
+    "url": "/clothing-shop/static/js/runtime~main.719515eb.js"
+  }
+]);
